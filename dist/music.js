1e18e2278993b8693430062a778c2c4646fdd156
Hiter-wh <1011412062@qq.com>
2021-10-01 17:42:53 +0800

    Site updated: 2021-10-01 17:42:53

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,19 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    mini: true,
-    autoplay: true,
-    theme: '#b7daff',
-    loop: 'all',
-    order: 'list',
-    preload: 'auto',
-    volume: 0.7,
-    mutex: true,
-    listFolded: false,
-    listMaxHeight: '200px',
-    lrcType: 3,
-    audio: [
-      {
-        name: "红色高跟鞋",
-        artist: '我亲爱的乐',
-        url: 'music/1.mp3',
-        cover: '/images/002.jpg',
-        //lrc: '/music/lyrics/兩隻老虎.lrc',
-      },
-    ]
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  audio: [
+{
+      name: "红色高跟鞋",
+      artist: '我亲爱的乐',
+      url: 'music/1.mp3',
+      cover: 'music/cover/002.jpg',
+    },
+    {
+      name: "平凡之路",
+      artist: '朴树',
+      url: 'http://www.ytmp3.cn/down/59211.mp3',
+      cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+    }
+  ]
 });
-
