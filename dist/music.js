a59f67332dafba619447cec3418feb1bb8bbec6b
Hiter-wh <1011412062@qq.com>
2021-10-16 10:04:35 +0800

    Site updated: 2021-10-16 10:04:34

--- dist/music.js
+++ dist/music.js
@@ -26,6 +26,6 @@ const ap = new APlayer({
       artist: '朴树',
       url: 'http://www.ytmp3.cn/down/59211.mp3',
       cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
-    }
+    },
   ]
 });
