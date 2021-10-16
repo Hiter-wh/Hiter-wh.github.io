c82446ced7e3f833dc6e4fa6e84d5c4ecfbe452b
Hiter-wh <1011412062@qq.com>
2021-10-16 10:00:34 +0800

    Site updated: 2021-10-16 10:00:33

--- dist/music.js
+++ dist/music.js
@@ -3,10 +3,24 @@ const ap = new APlayer({
   fixed: true,
   autoplay: false,
   audio: [
-{
+
+
+    {
       name: "红色高跟鞋",
       artist: '我亲爱的乐',
-      url: 'music/1.mp3',
+      url: 'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBZ05EOGY0cHhjSXZpQjVxQUR3RWtkOFVQV2pVP2U9cjhDMnZB.mp3',
+      cover: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9xZnF5LW15LnNoYXJlcG9pbnQuY29tLzppOi9yL3BlcnNvbmFsLzEwMTE0MTIwNjJfYTFfYjRfY20vRG9jdW1lbnRzL1N0b3JhZ2VfZm9yX2xpbmsvMDAyLmpwZz9jc2Y9MSZ3ZWI9MSZlPURJWmlCTA.jpg',
+    },
+    {
+      name: "红色高跟鞋2.0",
+      artist: '我亲爱的乐',
+      url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9xZnF5LW15LnNoYXJlcG9pbnQuY29tLzp1Oi9yL3BlcnNvbmFsLzEwMTE0MTIwNjJfYTFfYjRfY20vRG9jdW1lbnRzL1N0b3JhZ2VfZm9yX2xpbmsvMi5tcDM_Y3NmPTEmd2ViPTEmZT1WOGhRQnU.mp3',
+      cover: 'music/cover/002.jpg',
+    },
+    {
+      name: "红色高跟鞋新年版",
+      artist: '我亲爱的乐',
+      url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly9xZnF5LW15LnNoYXJlcG9pbnQuY29tLzp1Oi9yL3BlcnNvbmFsLzEwMTE0MTIwNjJfYTFfYjRfY20vRG9jdW1lbnRzL1N0b3JhZ2VfZm9yX2xpbmsvMy5tcDM_Y3NmPTEmd2ViPTEmZT1NYmZYRXg.mp3',
       cover: 'music/cover/002.jpg',
     },
     {
@@ -14,13 +28,6 @@ const ap = new APlayer({
       artist: '朴树',
       url: 'http://www.ytmp3.cn/down/59211.mp3',
       cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
-    },
-
-    {
-      name: "红色高跟鞋",
-      artist: 'Carol',
-      url: 'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBZ05EOGY0cHhjSXZpQjVxQUR3RWtkOFVQV2pVP2U9cjhDMnZB.mp3',
-    //  cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
     }
   ]
 });
